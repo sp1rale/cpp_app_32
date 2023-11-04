50c099d6a2e6e2119f827c1e724117f520b4b5de
sp1rale <dimayaremchyk24@gmail.com>
2023-11-04 18:28:55 +0200

    done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	double sides[5];
+	double perimeter = 0.0;
+	cout << "Enter the length of the sides of the pentagon:\n ";
+	for (int i = 0; i < 5; i++) {
+		cout << "Side " << i + 1 << ": ";
+		cin >> sides[i];
+		perimeter += sides[i];
+	}
+	cout << "The perimeter of the pentagon is " << perimeter << endl;
+
 	return 0;
 }
