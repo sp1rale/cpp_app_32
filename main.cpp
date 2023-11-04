f18986f4e6ed8ff9c613631fcd40a0bf4850eb7e
sp1rale <dimayaremchyk24@gmail.com>
2023-11-04 18:23:35 +0200

    done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+    double profits[6];
+    double totalProfit = 0.0;
+
+    for (int i = 0; i < 6; i++) {
+        cout << "Enter profit for " << i + 1 << "months: ";
+        cin >> profits[i];
+        totalProfit += profits[i];
+    }
+
+    cout << "Total profit for 6 months: " << totalProfit << endl;
+
 	return 0;
 }
