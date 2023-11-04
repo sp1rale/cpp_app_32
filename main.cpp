311333d206f91959ccbcd1c22fedaa6e8af9b4f9
sp1rale <dimayaremchyk24@gmail.com>
2023-11-04 18:30:08 +0200

    done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,27 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+    double profits[12];
+    int maxMonth = 0;
+    int minMonth = 0;
+
+    cout << "Enter profit for 12 months:" << endl;
+
+    for (int i = 0; i < 12; i++) {
+        cout << "Profit for " << i + 1 << "months: ";
+        cin >> profits[i];
+
+        if (profits[i] > profits[maxMonth]) {
+            maxMonth = i;
+        }
+
+        if (profits[i] < profits[minMonth]) {
+            minMonth = i;
+        }
+    }
+
+    cout << "The month with maximum profit: " << maxMonth + 1 << endl;
+    cout << "A month with minimal profit: " << minMonth + 1 << endl;
+
 	return 0;
 }
