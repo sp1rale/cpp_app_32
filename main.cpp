95f292b18f4d418005105e5902825f6e5fa887dc
sp1rale <dimayaremchyk24@gmail.com>
2023-11-04 18:26:01 +0200

    done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,20 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	int n;
+	cout << "Enter the array size: ";
+	cin >> n;
+	int* arr = new int[n];
+	cout << "Enter the array elements: ";
+	for (int i = 0; i < n; i++) {
+		cout << "Element " << i + 1 << ": ";
+		cin >> arr[i];
+	}
+	cout << "The array is in reverse order: ";
+	for (int i = n - 1; i >= 0; i--) {
+		cout << arr[i] << " ";
+	}
+	delete[] arr;
+
 	return 0;
 }
